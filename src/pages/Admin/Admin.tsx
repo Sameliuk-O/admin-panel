381fea9383953dd69c85cade18a8df0e881290c4
olegsameluk <olegsameluk82@gmail.com>
2023-08-10 14:32:13 +0300

    Feat: Create Category component and display in all pages

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -2,12 +2,10 @@ import { useEffect } from 'react';
 
 import { Previews } from '../../components/Previews';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
-import { useAppSelector } from '../../hooks/useAppSelector';
 import Data from '../../mock/feed.json';
 import { setCategory, setPreviews } from '../../store/templatesSlice';
 const Admin = () => {
   // const { data } = useGetAllTemplatesQuery();
-  const category = useAppSelector((state) => state.template.categories);
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(setCategory(Data?.categories));
@@ -15,7 +13,6 @@ const Admin = () => {
   }, []);
   return (
     <div className="container mx-auto pl-10 pt-10">
-      <ul>{category && category?.map((el) => <li key={el.categoryID}>{el.name}</li>)}</ul>
       <Previews />
     </div>
   );
--- src/routes/RoutesComponent/RoutesComponent.tsx
+++ src/routes/RoutesComponent/RoutesComponent.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { Category } from '../../components/Category';
 import { Header } from '../../components/Header';
 import { Admin } from '../../pages/Admin';
 
@@ -7,10 +8,13 @@ const RoutesComponent = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route element={<Admin />} path="/admin" />
-        <Route element={<Navigate to="/admin" replace />} path="/*" />
-      </Routes>
+      <div className=" container mx-auto flex">
+        <Category />
+        <Routes>
+          <Route element={<Admin />} path="/admin" />
+          <Route element={<Navigate to="/admin" replace />} path="/*" />
+        </Routes>
+      </div>
     </>
   );
 };
